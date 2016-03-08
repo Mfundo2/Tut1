617fbbfe69875f9a1cf11ecdb495aabd2bb348a4
mfundo2 <214527393@stu.ukzn.ac.za>
2016-03-08 13:46:29 +0100

    This is the simple fizz bizz wolf program

---
+++ Tut1-FizzBizzWolf/Tut1-FizzBizzWolf/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i = 1;//Intitializing the counter (i) from 1
+	for (i; i <= 50; i++)//Looping from 1 to 50 with an increment of 1
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+			cout << "Fizz" << endl;
+		else if (i % 5 == 0 && (i % 3) != 0)
+			cout << "Bizz" << endl;
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			cout << "Fizz and Bizz" << endl;
+		if (i % 7 == 0 && (i % 5) != 0 && (i % 3) != 0)
+			cout << "Wolf" << endl;
+		else if ((i % 7 == 0) && (i % 3) == 0 && (i % 5) == 0)
+			cout << "Fizz , Bizz and wolf " << endl;
+	}
+	cin.get();
+}
